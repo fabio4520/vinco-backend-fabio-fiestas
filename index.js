9150d1a5f25841feda493d49d159c4c179652c3f
fabio4520 <fabioleofc@gmail.com>
2022-10-22 17:45:07 -0500

    ADD: queries for crud products and categories Problem - needs to add crud for products - Categories table wasn't need to be created. Categories names were selected by a querie. Solution - Connect postgresql with nodejs - Create routes for our simple crud - Add global env for connect with database.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const PORT = 3000
+const PORT = 8888
 const HOST = 'localhost'
+const db = require('./queries')
 
 app.use(bodyParser.json())
 app.use(
@@ -11,9 +12,38 @@ app.use(
   })
 ) 
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get('/', (request, response) => {
-  response.json({ info: 'Backedn for Vinco challenge' })
+  response.json({ info: 'Backend for Vinco challenge' })
 })
+
+app.get('/products', db.getProducts)
+app.get('/products/:id', db.getProductById)
+app.post('/products', db.createProduct)
+app.put('/products/:id', db.updateProduct)
+app.delete('/products/:id', db.deleteProduct)
+app.get('/categories', db.getCategories)
+app.get('/categories/:name', db.getCategory)
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://${HOST}:${PORT}`)
---
+++ queries.js
@@ -0,0 +1,124 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: 'vinco',
+  password: 'password',
+  port: 5432
+})
+
+const getProducts = (request, response) => {
+  pool.query('SELECT * FROM products ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getProductById = (request, response) => {
+  const id = parseInt(request.params.id);
+  pool.query(`SELECT * from products where id = ${id}`, (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows[0])
+  })
+}
+
+const createProduct = (request, response) => {
+  const { 
+    title,
+    price,
+    category,
+    description,
+    image,
+    rating
+   } = request.body
+  console.log(request.body)
+  pool.query('INSERT INTO products (title, price, category, description, image, rating) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [
+    title,
+    price,
+    category,
+    description,
+    image,
+    rating
+  ], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(`Product added with ID: ${results.rows[0].id}`)
+  })
+}
+
+const updateProduct = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { 
+    title,
+    price,
+    category,
+    description,
+    image,
+    rating
+   } = request.body
+
+  pool.query(
+    'UPDATE products SET title = $1, price = $2, category = $3, description = $4, image = $5, rating = $6  WHERE id = $3',
+    [
+      title,
+      price,
+      category,
+      description,
+      image,
+      rating
+    ],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Product modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteProduct = (request, response) => {
+  const id = parseInt(request.params.id)
+  pool.query(`delete from products where id = ${id}`, (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(`Product deleted with ID: ${id}`)
+  })
+}
+
+// Categories
+const getCategories = (request, response) => {
+  pool.query('SELECT DISTINCT category from products', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(
+      results.rows.map((r) => r.category)
+    )
+  })
+}
+// products by category
+const getCategory = (request, response) => {
+  const name = request.params.name;
+  pool.query('SELECT * from products where category = $1', [name], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+module.exports = {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getCategories,
+  getCategory
+}
